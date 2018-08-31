e2b67f5be6185b9e8cbb320d31d2d1216b17ff5c
Kailin Huang <kailin-huang@outlook.com>
2018-08-31 18:23:50 +0200

    changed VBH sizes to a small number but works with coarse resolution of voxels

--- InfiniTAM/ITMLib/Objects/Scene/ITMVoxelBlockHash.h
+++ InfiniTAM/ITMLib/Objects/Scene/ITMVoxelBlockHash.h
@@ -15,10 +15,10 @@
 #define SDF_BLOCK_SIZE 8				// SDF block size
 #define SDF_BLOCK_SIZE3 512				// SDF_BLOCK_SIZE3 = SDF_BLOCK_SIZE * SDF_BLOCK_SIZE * SDF_BLOCK_SIZE
 
-#define SDF_LOCAL_BLOCK_NUM 0x20000		// 0x40000 0x20000 0x10000 Number of locally stored blocks, currently 2^17
-#define SDF_BUCKET_NUM 0x80000		// 0x100000 0x80000 0x40000 Number of Hash Bucket, should be 2^n and bigger than SDF_LOCAL_BLOCK_NUM, SDF_HASH_MASK = SDF_BUCKET_NUM - 1
-#define SDF_HASH_MASK 0x7ffff		// 0xfffff 0x7ffff 0x3ffff Used for get hashing value of the bucket index,  SDF_HASH_MASK = SDF_BUCKET_NUM - 1
-#define SDF_EXCESS_LIST_SIZE 0x10000	// 0x20000 0x10000 0x8000 Size of excess list, used to handle collisions. Also max offset (unsigned short) value.
+#define SDF_LOCAL_BLOCK_NUM 0x10000		// 0x40000 0x20000 0x10000 Number of locally stored blocks, currently 2^17
+#define SDF_BUCKET_NUM SDF_LOCAL_BLOCK_NUM*4		// 0x100000 0x80000 0x40000 Number of Hash Bucket, should be 2^n and bigger than SDF_LOCAL_BLOCK_NUM, SDF_HASH_MASK = SDF_BUCKET_NUM - 1
+#define SDF_HASH_MASK SDF_BUCKET_NUM-1		// 0xfffff 0x7ffff 0x3ffff Used for get hashing value of the bucket index,  SDF_HASH_MASK = SDF_BUCKET_NUM - 1
+#define SDF_EXCESS_LIST_SIZE 0x8000	// 0x20000 0x10000 0x8000 Size of excess list, used to handle collisions. Also max offset (unsigned short) value.
 
 #define SDF_LOCAL_BLOCK_NUM_BG 0x40000
 #define SDF_BUCKET_NUM_BG 0x100000
